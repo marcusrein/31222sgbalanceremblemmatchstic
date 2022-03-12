b8748964b3c36f42157184dc82b3e318f8baf538
Marcus Rein <64141593+marcusrein@users.noreply.github.com>
2022-03-12 12:09:27 -0500

    First test working!
    Got the first test to work. Matchstick implemented.

--- tests/demo.test.ts
+++ tests/demo.test.ts
@@ -1,12 +1,12 @@
 import { SuperBadgeGRT } from "./../generated/schema";
 import { clearStore, test, assert } from "matchstick-as/assembly/index";
+import { bigInt } from "@graphprotocol/graph-ts";
+
+let SUPERBADGE_ENTITY_TYPE = "SuperBadgeGRT";
 
 test("See if a handler fills up a SuperBadgeGRT", () => {
-	let x = BigInt(99999);
-
 	let bob = new SuperBadgeGRT("11111");
-	bob.value = x;
 	bob.save();
 
-	assert.fieldEquals("SuperBadgeGrt", "11111", "value", null);
+	assert.fieldEquals(SUPERBADGE_ENTITY_TYPE, "11111", "id", "11111");
 });
