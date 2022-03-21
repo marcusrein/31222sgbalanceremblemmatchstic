44fb130a8ad6c03c097b5991525c7bbe4896a40e
Marcus Rein <64141593+marcusrein@users.noreply.github.com>
2022-03-21 10:14:57 -0400

    got to 'failed test'!
    still buggy tho

--- tests/wip.test.ts
+++ tests/wip.test.ts
@@ -4,23 +4,34 @@ import { newMockEvent, assert, test } from "matchstick-as/assembly/index";
 import { handleApproval } from "../src/mapping";
 import { Approval } from "../generated/Contract/Contract";
 
-export function createNewApprovalEvent(id: i32, value: string): Approval {
-	let newApprovalEvent = newMockEvent();
+export function createNewApprovalEvent(
+	owner: i32,
+	spender: string,
+	value: string
+): Approval {
+	let newApprovalEvent = changetype<Approval>(newMockEvent());
+
 	newApprovalEvent.parameters = new Array();
-	let idParam = new ethereum.EventParam("id", ethereum.Value.fromI32(id));
+
+	let newOwner = new ethereum.EventParam("id", ethereum.Value.fromI32(owner));
+	let newSpender = new ethereum.EventParam(
+		"id",
+		ethereum.Value.fromString(spender)
+	);
 	let valueParam = new ethereum.EventParam(
 		"value",
 		ethereum.Value.fromString(value)
 	);
 
-	newApprovalEvent.parameters.push(idParam);
+	newApprovalEvent.parameters.push(newOwner);
 	newApprovalEvent.parameters.push(valueParam);
+	newApprovalEvent.parameters.push(newSpender);
 
 	return newApprovalEvent;
 }
 
 test("Can handle new Approval event", () => {
-	let newApprovalEvent = createNewApprovalEvent(0x1234, "9999");
+	let newApprovalEvent = createNewApprovalEvent(0x1234, "9999", "8888");
 
 	handleApproval(newApprovalEvent);
 
